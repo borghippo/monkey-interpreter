a85b33428fb42c972cb6b69a62401772677bccbb
atborg <33327789+atborg@users.noreply.github.com>
2022-11-07 21:15:59 -0700

    add !-/*<> tokens to lexer

--- token/token.go
+++ token/token.go
@@ -9,8 +9,15 @@ const (
 	IDENT = "IDENT"
 	INT   = "INT"
 	// Operators
-	ASSIGN = "="
-	PLUS   = "+"
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+
+	LT = "<"
+	GT = ">"
 	// Delimiters
 	COMMA     = ","
 	SEMICOLON = ";"
